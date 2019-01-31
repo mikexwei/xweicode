f20468ebc099ee94d50785ca95ca9b0ba5496b1a
Mike <mikexwei@gmail.com>
2019-01-30 21:17:20 -0800

    RemoveDuplidatesFromSortedArray

--- src/main/java/com/personal/xwei/leetcode/_0026RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/personal/xwei/leetcode/_0026RemoveDuplicatesFromSortedArray.java
@@ -1,13 +1,21 @@
 package com.personal.xwei.leetcode;
 
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ */
 public class _0026RemoveDuplicatesFromSortedArray {
   public int removeDuplicates(int[] nums) {
     int len = nums.length;
     int i = 0;
-    int ans = 0;
+    if (len == 0) return 0;
     for (int p = 0; p < len; p++) {
-      if (nums[i] < nums[p]) nums[++i] = nums[p];
+      if (i == len - 1) return i + 1;
+      if (nums[i] < nums[p]) {
+        nums[++i] = nums[p];
+      }
     }
-    return i;
+    return i + 1;
   }
 }
