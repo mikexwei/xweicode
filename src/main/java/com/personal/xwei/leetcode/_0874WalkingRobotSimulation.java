24af46d76635cf8099509d4a0dca2a89424d982c
Mike <mikexwei@gmail.com>
2019-02-11 00:15:55 -0800

    874

--- src/main/java/com/personal/xwei/leetcode/_0874WalkingRobotSimulation.java
+++ src/main/java/com/personal/xwei/leetcode/_0874WalkingRobotSimulation.java
@@ -1,4 +1,51 @@
 package com.personal.xwei.leetcode;
 
+import java.util.*;
+
 public class _0874WalkingRobotSimulation {
+  public static int robotSim(int[] commands, int[][] obstacles) {
+    int[][] d_map = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int d = 0;
+    int[] position = {0, 0};
+    int result = 0;
+    for (int i = 0; i < commands.length; i++) {
+      if (commands[i] == -1) d++;
+      else if (commands[i] == -2) d--;
+      else {
+        while (d < 0) d += 4;
+        d %= 4;
+        if (d_map[d][0] == 0) {
+          int old_y = position[1];
+          int new_y = position[1] + commands[i] * d_map[d][1];
+          for (int j = 0; j < obstacles.length; j++) {
+            if (obstacles[j][0] == position[0] && (obstacles[j][1] <= new_y && obstacles[j][1] > old_y))
+              new_y = obstacles[j][1] - 1;
+            if (obstacles[j][0] == position[0] && (obstacles[j][1] >= new_y && obstacles[j][1] < old_y))
+              new_y = obstacles[j][1] + 1;
+          }
+          position[1] = new_y;
+        }
+        if (d_map[d][1] == 0) {
+          int old_x = position[0];
+          int new_x = position[0] + commands[i] * d_map[d][0];
+          for (int k = 0; k < obstacles.length; k++) {
+            if (obstacles[k][1] == position[1] && (obstacles[k][0] <= new_x && obstacles[k][0] > old_x))
+              new_x = obstacles[k][0] - 1;
+            if (obstacles[k][1] == position[1] && (obstacles[k][0] >= new_x && obstacles[k][0] < old_x))
+              new_x = obstacles[k][0] + 1;
+          }
+          position[0] = new_x;
+        }
+      }
+      result = Math.max(result, position[0] * position[0] + position[1] * position[1]);
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] c = {4, -1, 4, -2, 4};
+    int[][] o = {{2, 4}};
+    System.out.println(robotSim(c, o));
+
+  }
 }
--- src/test/java/leetcode/_0000TestTemplate.java
+++ src/test/java/leetcode/_0000TestTemplate.java
@@ -3,6 +3,9 @@ package leetcode;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class _0000TestTemplate {
   @DataProvider(name = "dp")
   public Object[][] dataProvider() {
@@ -16,6 +19,21 @@ public class _0000TestTemplate {
   @Test(dataProvider = "dp")
   public void test() {
 
+
+  }
+
+  public static void main(String[] args) {
+    String s = "123             ";
+//    String reg = "^[0-9].";
+//    Pattern p = Pattern.compile(reg);
+//    Matcher m = p.matcher(s);
+////    while (m.find()) {
+////      System.out.println(m.group(0));
+////    }
+//    String[] s1 = s.split("\\D");
+//    String s2 = s.substring(1);
+    String[] s1 = s.split("^\\s+");
+    System.out.println(s.split("^\\s+")[0]);
 
   }
 }
